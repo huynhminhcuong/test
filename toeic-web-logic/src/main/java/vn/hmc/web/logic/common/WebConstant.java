6f2d9e5a0905bf2d62a2da3c154d50a0101d253a
cuong <minhcuong51020@gmail.com>
2021-02-05 12:30:07 +0700

    test

---
+++ toeic-web-logic/src/main/java/vn/hmc/web/logic/common/WebConstant.java
@@ -0,0 +1,6 @@
+package vn.hmc.web.logic.common;
+
+// Các hang số chỉ sử dung trong controller
+public class WebConstant {
+
+}
---
+++ toeic-web/src/main/webapp/index.jsp
@@ -0,0 +1,8 @@
+<html>
+<head>
+    <title>test project</title>
+</head>
+<body>
+    <h1>test</h1>
+</body>
+</html>
